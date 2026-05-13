4b50fa57d307f6f9746fdc9143376a0c2dc28fc4
Miroslav Slouka <163758455+MiroslavSlouka1@users.noreply.github.com>
2026-05-13 10:30:11 +0200

    Refactor tests to use Page parameter instead of custom_page
    Uprava kodu na zaklade pripominek lektora

--- test_engeto.py
+++ test_engeto.py
@@ -1,48 +1,41 @@
-from playwright.sync_api import Page, expect, sync_playwright
 import pytest
+from playwright.sync_api import Page, expect
 
-
-@pytest.fixture(scope="session")
-def custom_page():
-    with sync_playwright() as p:
-        browser = p.firefox.launch(headless=False, slow_mo=2000)
-        page = browser.new_page()
-        yield page
 
 # Test zobrazení title
-def test_title(custom_page):
-    custom_page.goto("https://engeto.cz/")
+def test_title(page: Page):
+    page.goto("https://engeto.cz/")
     expected_title = "Kurzy programování a dalších IT technologií | ENGETO"
-    expect(custom_page).to_have_title(expected_title)
+    expect(page).to_have_title(expected_title)
 
-# Test click na cookis
-def test_cookisclick(custom_page):
-    custom_page.goto("https://engeto.cz/")
-    custom_page.click("#cookiescript_accept")
-    expect(custom_page.locator("#cookiescript_injected")).to_be_hidden()
 
-# Test zadaní špatného mailu
-def test_bad_mail_input(custom_page):
-    custom_page.goto("https://engeto.cz/")
+# Test click na cookies
+def test_cookies_click(page: Page):
+    page.goto("https://engeto.cz/")
+    page.click("#cookiescript_accept")
+    expect(page.locator("#cookiescript_injected")).to_be_hidden()
+
+
+# Test zadání špatného mailu
+def test_bad_mail_input(page: Page):
+    page.goto("https://engeto.cz/")
 
     # Cookies
-    accept_button = custom_page.locator("#cookiescript_accept")
+    accept_button = page.locator("#cookiescript_accept")
     if accept_button.is_visible():
         accept_button.click()
 
     # Neplatný email
-    email_input = custom_page.get_by_placeholder("Zadej svůj e-mail")
+    email_input = page.get_by_placeholder("Zadej svůj e-mail")
     expect(email_input).to_be_visible()
     email_input.fill("1111")
 
-    # Klik na "Odebírat" 
-    subscribe_button = custom_page.get_by_role("link", name="Odebírat")
+    # Klik na "Odebírat"
+    subscribe_button = page.get_by_role("link", name="Odebírat")
     expect(subscribe_button).to_be_visible()
     subscribe_button.click()
 
     # Ověření chybové hlášky
-    error_message = custom_page.locator("span.error-message")
+    error_message = page.locator("span.error-message")
     expect(error_message).to_be_visible()
-    expect(error_message).to_have_text(
-        "Prosím zadejte validní emailovou adresu"
-    )
+    expect(error_message).to_have_text("Prosím zadejte validní emailovou adresu")
